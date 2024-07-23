e511ce6fb17f19f55526f0519f43c66ca6c0d68e
Huzaifa Iqbal <huzaifa.iqdev@gmail.com>
2024-07-24 02:00:31 +0500

    max duration

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,7 +2,8 @@ import { ActionFunctionArgs, json, type MetaFunction } from "@remix-run/node";
 import Papa from "papaparse";
 import twilio from "twilio";
 import CsvUpload from "~/components/CsvUpload";
-
+export const maxDuration = 20;
+ 
 export const meta: MetaFunction = () => {
   return [
     { title: "New Remix App" },
