6d6de74146c8390e7203b9364d28f9010b2caf35
Huzaifa Iqbal <huzaifa.iqdev@gmail.com>
2024-07-24 02:17:50 +0500

    max duration vercel

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,9 +2,14 @@ import { ActionFunctionArgs, json, type MetaFunction } from "@remix-run/node";
 import Papa from "papaparse";
 import twilio from "twilio";
 import CsvUpload from "~/components/CsvUpload";
-
-export const maxDuration = 20;
+export const maxDuration: number = 60;
 export const dynamic = 'force-dynamic';
+ 
+export function GET(request: Request) {
+  return new Response('Vercel', {
+    status: 200,
+  });
+}
 
 export const meta: MetaFunction = () => {
   return [
@@ -20,15 +25,21 @@ export async function action({ request }: ActionFunctionArgs) {
   const formData = await request.formData();
   const fileContent = formData.get("fileContent") as string;
   const type = formData.get("type") as string;
+  
 
+  // Extract CSV fields
   if (type === "parse-fields") {
     function getCsvFields(content: string): Promise<string[]> {
       return new Promise((resolve, reject) => {
+        const csvFields: any[] = [];
         Papa.parse(content, {
           header: true,
+          dynamicTyping: true,
           complete: (results: any) => {
             const fields = Object.keys(results.data[0]);
-            resolve(fields);
+            csvFields.push(...fields);
+            // will return csvFields as array of string (Promise<string[]>)
+            resolve(csvFields);
           },
           error: (error: any) => {
             reject(error);
@@ -37,16 +48,19 @@ export async function action({ request }: ActionFunctionArgs) {
       });
     }
     const parsedCsvFields = await getCsvFields(fileContent);
-    return json({ parsedCsvFields });
+    return json({
+      parsedCsvFields,
+    });
   } else {
     const selectedField = formData.get("selectedField") as string;
-
+    const fileContent = formData.get("fileContent") as string;
     const phoneNumbers: string[] = [];
     Papa.parse(fileContent, {
       header: true,
       dynamicTyping: true,
       skipEmptyLines: true,
       complete: (results) => {
+        console.log("SW result in parsing", results);
         results.data.forEach((row: { [key: string]: any }) => {
           const number = row[selectedField];
           if (number) {
@@ -56,41 +70,44 @@ export async function action({ request }: ActionFunctionArgs) {
       },
     });
 
-    async function processPhoneNumbers(numbers: string[]) {
-      const results = [];
-      for (const number of numbers) {
-        try {
-          const response = await client.lookups.v2.phoneNumbers(number).fetch({ fields: "line_type_intelligence" });
-          results.push({ number, ...response.lineTypeIntelligence });
-        } catch (error) {
-          console.log("Error: ", error);
-        }
-        await delay(100); // Ensure this is outside the try-catch to avoid blocking on errors
-      }
-      return results;
+    // initiate delay to overcome (too many requests error in Twilio API)
+    function delay(ms: number) {
+      return new Promise((resolve) => setTimeout(resolve, ms));
     }
 
-    const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
-    const chunkSize = 50;
-    let results = [];
-
-    for (let i = 0; i < phoneNumbers.length; i += chunkSize) {
-      const chunk = phoneNumbers.slice(i, i + chunkSize);
-      const chunkResults = await processPhoneNumbers(chunk);
-      results = results.concat(chunkResults);
-
-      // Check if the total execution time is approaching the limit
-      if (Date.now() - startTime > (maxDuration - 1) * 1000) {
-        break;
+    const results = [];
+    for (const number of phoneNumbers) {
+      try {
+        const response = await client.lookups.v2
+          .phoneNumbers(number)
+          .fetch(
+            {fields: "line_type_intelligence"},
+          );
+        results.push({ number, ...response.lineTypeIntelligence });
+        // const isMobile: boolean = response.lineTypeIntelligence.type === "mobile"
+        // if (isMobile) {
+        //   results.push({
+        //     number, lineTypeIntelligence:
+        //     ...response.lineTypeIntelligence,
+        //   });
+        // }
+      } catch (error) {
+        console.log("Error : ", error);
+        // results.push({ number, error: (error as Error).message });
       }
+      await delay(100);
     }
-
+    console.log("SW results", results);
     const csv = Papa.unparse(results);
-    return json({ results, csv });
+    return json({
+      results,
+      csv,
+    });
   }
 }
 
 export default function Index() {
+
   return (
     <div className="h-screen flex justify-center items-center">
       <CsvUpload />
